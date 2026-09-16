10d9817ae4054549e99c12b02da82a1435137518
wafa <w.zenas@icloud.com>
2026-09-16 09:35:26 +0200

    changed operation method

--- fonctions.py
+++ fonctions.py
@@ -1,7 +1,10 @@
 def puissance(a, b):
+	res=a
 	if not type(a) is int and not type(b) is int:
 		raise TypeError("Only integers are allowed") #inutile car input a besoin de typecasting pour fonctionner
 	if a == 0 and b < 0:
 		raise ValueError("0 cannot have a negative exponent")
 	else:
-		return a**b
+		for i in range(b+1):
+			res*=a
+		return res
