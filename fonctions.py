998488efa1be4a3e12f66897714687ddc4b158e6
wafa <w.zenas@icloud.com>
2026-09-16 09:28:06 +0200

    fixed syntax

--- fonctions.py
+++ fonctions.py
@@ -2,6 +2,6 @@ def puissance(a, b):
 	if not type(a) is int and not type(b) is int:
 		raise TypeError("Only integers are allowed") #inutile car input a besoin de typecasting pour fonctionner
 	if a == 0 and b < 0:
-        raise ValueError("0 cannot have a negative exponent")
+		raise ValueError("0 cannot have a negative exponent")
 	else:
 		return a**b
