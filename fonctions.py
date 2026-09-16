851d21b6bb914ed8881eda20cfc5f0da60329022
wafa <w.zenas@icloud.com>
2026-09-16 11:13:34 +0200

    correctifs rqise excetion

--- fonctions.py
+++ fonctions.py
@@ -2,7 +2,7 @@ def puissance(a, b):
 	res=1
 	if not type(a) is int and not type(b) is int:
 		raise TypeError("Only integers are allowed") #inutile car input a besoin de typecasting pour fonctionner
-	if a == 0 and b < 0:
+	if a == 0 and b <= 0:
 		raise ValueError("0 cannot have a negative exponent")
 	else:
 		if b>0:
