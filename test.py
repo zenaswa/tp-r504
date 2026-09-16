faf7b51f66715f6d47ecfdd8c3fb2ec3170fbf22
wafa <w.zenas@icloud.com>
2026-09-16 09:17:13 +0200

    0powered test check

--- test.py
+++ test.py
@@ -10,3 +10,8 @@ def test_2():
 	assert f.puissance(-1,3) == -1
 	assert f.puissance(-1,-2) == 1
 	assert f.puissance(-2,-1) == -0.5
+
+def test_3():
+	assert f.puissance(0,8) == 0
+	with pytest.raises(Exception):
+		f.puissance(0,-4)
