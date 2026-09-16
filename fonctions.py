731f2a3b8374ae35a749eafbf2bb546ff44822b3
wafa <w.zenas@icloud.com>
2026-09-16 09:23:34 +0200

    added expcetion raising

--- fonctions.py
+++ fonctions.py
@@ -1,5 +1,6 @@
 def puissance(a, b):
 	if not type(a) is int and not type(b) is int:
 		raise TypeError("Only integers are allowed") #inutile car input a besoin de typecasting pour fonctionner
-	else:
-		return a**b
+	if a == 0 and b < 0:
+        raise ValueError("0 cannot have a negative exponent")
+	return a**b
